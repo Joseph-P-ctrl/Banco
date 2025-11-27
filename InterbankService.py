edb10d94b2de0361c83e9ac0c38e6250adb2a101
Aldo Eslyn Abril Ricalde <aldoabril@gmail.com>
2025-11-27 15:13:42 -0500

    fix: campo moneda

--- InterbankService.py
+++ InterbankService.py
@@ -31,12 +31,29 @@ class InterbankService:
                 raise MyCustomException("Archivo Interbanks: Columnas no encontradas, elimine cabeceras innecesarias")
             
             df_interbancarias["Monto abonado"] = df_interbancarias["Monto abonado"].astype(str).str.replace(",", "")
+            print(df_interbancarias["Monto abonado"])
             df_interbancarias["Monto abonado"] = pd.to_numeric(df_interbancarias["Monto abonado"],errors='coerce')
+            print(df_interbancarias["Monto abonado"])
+            print(self.movimientos)
+
+            # Limpia la columna: quita espacios internos y externos
+            df_interbancarias["Monto abonado - Moneda"] = (
+                df_interbancarias["Monto abonado - Moneda"]
+                .str.strip()           # elimina espacios al inicio y al final
+                .str.replace(" ", "")  # elimina espacios internos
+            )
+            print('antes de filtrar', df_interbancarias)
+            df_interbancarias = df_interbancarias.loc[df_interbancarias["Monto abonado - Moneda"] =="S/"].copy()
+            print('luego de filtrar', df_interbancarias)
             
-            df_interbancarias = df_interbancarias.loc[df_interbancarias["Monto abonado - Moneda"]=="S/"].copy()
+            
+
 
             for index, row in df_interbancarias.iterrows():
                 num_operacion = str(int(row["N° Operación"]))
+                print('numero operacion',num_operacion)
+                operacion_numero=  self.movimientos["Operación - Número"].astype(str).str[-4:]
+                print('operacion_numero', operacion_numero)
                 reg = self.movimientos.loc[(self.movimientos["Monto"].apply(lambda x: round(x, 2))==round(row["Monto abonado"],2)) &
                                            (self.movimientos["Operación - Número"].astype(str).str[-4:]==num_operacion[-4:])].copy()
                 
